89aa21483b0fd6e6a640737a0e162c8af3d8c651
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-03-30 01:30:09 +0530

    Looping Task 16

---
+++ Looping/Task-16.js
@@ -0,0 +1,10 @@
+// How many times the highlighted text (bold and underlined) is being checked .
+// for( i=1;i<=5;i++)
+// {
+// for( j=1;j<=5;j++)
+// {
+// Code ...
+// }
+// }
+
+// 5*(5+1(false condition check in j loop))=5*6=30
