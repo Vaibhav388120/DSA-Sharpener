2c15d398a58b96919fcdc710e9f67ccdc91af3d4
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-03-30 00:00:28 +0530

    Basic Coding Task-3

---
+++ Basic Coding/Task-3.js
@@ -0,0 +1,12 @@
+// Write a program to print the largest of the three numbers.
+
+let a=3,b=5,c=1;
+if(a>b && a>c){
+    console.log(a);
+}
+else if(b>a && b>c){
+    console.log(b);
+}
+else{
+    console.log(c);
+}
