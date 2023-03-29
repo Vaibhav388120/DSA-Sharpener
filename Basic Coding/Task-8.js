655cf784f77375cf6f906dbfe7a5fc080b21a8db
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-03-30 00:14:12 +0530

    Basic Coding Task 8

---
+++ Basic Coding/Task-8.js
@@ -0,0 +1,24 @@
+// What will be the output of the following algorithm .
+
+// switch (x)
+// {
+// case 1:
+// print( "Choice is 1");
+// break;
+// case 2:
+// print( "Choice is 2");
+// case 3:
+// print("Choice is 3");
+// case 4:
+// print("Choice is 4");
+// break;
+// default:
+// print("Choice other than 1, 2, 3 and 4")
+// break;
+// }
+
+// When x=2, Output is Choice is 2
+// Choice is 3
+// Choice is 4
+
+//This is because break statement is missing
