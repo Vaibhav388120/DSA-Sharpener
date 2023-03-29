1a1b3e156fe44a5c9ebb60581b93a35c0166378a
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-03-30 00:07:13 +0530

    Basic Coding Task 5

---
+++ Basic Coding/Task-5.js
@@ -0,0 +1,4 @@
+// Write a program to assume a value of marks and print whether a person has failed or passed using ternary Operator.
+
+let marks=52;
+marks>=40?console.log('pass'):console.log('fail');
