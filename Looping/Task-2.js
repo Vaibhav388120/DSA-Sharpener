c37c7d9aee08d883ca1269df5901cfbe3454fa10
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-03-30 00:26:49 +0530

    flow of code

--- Looping/Task-2.js
+++ Looping/Task-2.js
@@ -6,3 +6,8 @@ while(i*i<=n){
     console.log(i*i);
     i++;
 }
+
+//i=1 i*i=1*1<=n true print 1*1=1 i++
+//i=2 i*i=2*2=4<=n true print 2*2=4 i++
+//i=3 i*i=3*3=9<=n true print 3*3=9 i++
+//so on till i*i<=n
---
+++ Looping/Task-3.js
@@ -0,0 +1,2 @@
+// By using while loop and continue statement
+//Print all number from 1 to n but then it should not be divisible by 5
