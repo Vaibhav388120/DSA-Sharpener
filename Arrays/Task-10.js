360bb9369b437555b2a9cdb7bc97a18f2d0abff9
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-03-30 16:20:14 +0530

    another approach to solve

--- Arrays/Task-10.js
+++ Arrays/Task-10.js
@@ -2,6 +2,30 @@
 // There is one element missing from the range[1,n] in the array. Find that missing Number and return it.
 
 let arr=[3,4,1];
+
+//approach 1
+// extra space O(N)
+let len=arr.length;
+let temp=[len+1];
+
+for(let i=0;i<=len;i++){
+    temp[i]=0;
+}
+for(let i=0;i<len;i++){
+    temp[arr[i]-1]=1;
+}
+
+let ans=0;
+for(let i=0;i<=len;i++){
+    if(temp[i]===0){
+        ans=i+1;
+    }
+}
+
+console.log(ans);
+
+//approach 2
+//probability of overflow when n is large
 let n=arr.length;
 let sum=parseInt((n+1)*(n+2)/2);
 let arr_sum=0,missing;
