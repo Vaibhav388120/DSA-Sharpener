837466a1a14c28a36373f3d3ca9a033a20a549d3
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-04-01 11:47:06 +0530

    Strings

---
+++ Bit Manipulation&T.C/Task-9.js
@@ -0,0 +1,27 @@
+//Strings
+
+// let text="vaibhav";
+// console.log("length="+text.length);
+
+//slice
+// let text="vaibhav";
+// let part=text.slice(1,4);
+// let part=text.slice(2);
+// let part=text.slice(-2); // 2 letters from backwards
+// console.log(part);
+
+//substring
+// let text="vaibhav";
+// let part=text.substring(3,-2);  // -ve values are taken 0
+// console.log(part);
+
+//substr
+// let text="vaibhav";
+// let part=text.substr(2,3); //second parameter indicates length
+// console.log(part);
+
+//replace
+// let text="vaibhav achegave";
+// let part=text.replace("vaibhav","kashinath");
+// console.log(part);
+
