1556c9be7d843f06820ca0a2132f27aa00dff2b0
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-04-01 09:01:34 +0530

    Bit Manipulation Task 3

---
+++ Bit Manipulation&T.C/Task-3.js
@@ -0,0 +1,20 @@
+// Given an integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
+
+// All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
+
+let num=26;
+
+const arr=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+let res="";
+if(num===0){
+    return "0";
+}
+if(num<0){
+    num=num+ 2**32;
+}
+while(num>0){
+    let rem=parseInt(num%16);
+    res=arr[rem]+res;
+    num=parseInt(num/16);
+}
+console.log(res);
