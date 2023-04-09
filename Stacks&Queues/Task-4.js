1d7cc6f7f56e0099b535f8e23c45e29e12a35699
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-04-10 00:38:14 +0530

    questions

--- Stacks&Queues/Task-4.js
+++ Stacks&Queues/Task-4.js
@@ -1,3 +1,5 @@
+// Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
+
 var validateStackSequences=function(pushed,popped){
     let stack=[];
     let i=0;
