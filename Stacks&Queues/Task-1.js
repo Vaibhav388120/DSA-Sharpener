353e23480c314129a7790c1343ebc5b8dd24963d
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-04-10 00:07:18 +0530

    Stacks Task-1

---
+++ Stacks&Queues/Task-1.js
@@ -0,0 +1,35 @@
+class Stack{
+    constructor(n){
+        this.stack=[];
+        this.size=n;
+        this.index=-1;
+    }
+    push(element){
+        if(this.index===this.size){
+            return false;
+        }
+        this.index++;
+        this.stack[this.index]=element;
+    }
+    pop(){
+        if(this.index===-1){
+            return false;
+        }
+        const popped=this.stack.pop();
+        this.index--;
+        return popped;
+    }
+    top(){
+        return this.stack[this.index];
+    }
+}
+
+const stack=new Stack(5);
+stack.push(1);
+stack.push(2);
+stack.push(3);
+const popped=stack.pop();
+const top=stack.top();
+console.log(popped);
+console.log(top);
+console.log(stack);
