15da68cdf2edec21ce8db0b7cae65a52a0a52ffa
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2023-04-10 11:23:41 +0530

    5 Assignment Questions

---
+++ Assignments/Task-2.js
@@ -0,0 +1,17 @@
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let output=[];
+    for(let i=0;i<n;i++){
+        output.push(nums[i],nums[i+n]);
+    }
+    return output;
+};
+
+console.log(shuffle([2,5,1,3,4,7],3));
---
+++ Assignments/Task-3.js
@@ -0,0 +1,29 @@
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max=candies[0],output=[];
+    for(let i=1;i<candies.length;i++){
+        if(candies[i]>max){
+            max=candies[i];
+        }
+    }
+    for(let i=0;i<candies.length;i++){
+        if(candies[i]+extraCandies>=max){
+            output.push(true);
+        }
+        else{
+            output.push(false);
+        }
+    }
+    return output;
+};
+console.log(kidsWithCandies([2,3,5,1,3],3));
